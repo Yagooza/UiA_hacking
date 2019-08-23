0731d161c3d5448eb8cb38ea77cb069b7e49aa54
vegart13 <35805073+vegart13@users.noreply.github.com>
2019-08-23 10:51:03 +0200

    Update IP_bash_script.sh

--- IP_bash_script.sh
+++ IP_bash_script.sh
@@ -5,7 +5,17 @@ echo hello world
 
 
 # Develop a bash script that takes an IP (IPv4) from a user. Based on this IP, the script should ping all (255) IPs within that range. For example, if the user enters the IP 192.168.1.50, the script should ping every IP from 192.168.1.1 to 192.168.1.255 *Hint:* use read to get input from the user.
+#!/bin/bash
+read -p "Enter beginning IP-address: " host1
+read -p "Enter ending IP-address: " host2
+SUBNET=${host1%.*}
+netId1=${host1#$SUBNET.}
+netId2=${host2#$SUBNET.}
+for ((i=netId1; i<=netId2; i++)); do
+ping -c 1 ${SUBNET}.$i
+done
 
+# Update the script so that the script only prints the IPs of which there was a reply. *Hint:* It might be easier to temporarily store the results in a file
 #!/bin/bash
 prefix=${1:-192.168.1}
 
@@ -14,7 +24,3 @@ do
   ( echo ping -c1 -w1 ${prefix}.${ip} > /dev/null 2>&1 && echo ${prefix}.${ip} ) &
 done
 wait
-
-# Update the script so that the script only prints the IPs of which there was a reply. *Hint:* It might be easier to temporarily store the results in a file
-
-#!/bin/bash
