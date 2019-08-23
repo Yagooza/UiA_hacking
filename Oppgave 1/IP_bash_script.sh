63da9e7f352ca8db1286631d08fb05fb05a4760a
Yaguel <yaguev17@hotmail.com>
2019-08-23 11:36:57 +0200

    Update IP_bash_script.sh

--- Oppgave 1/IP_bash_script.sh
+++ Oppgave 1/IP_bash_script.sh
@@ -1,10 +1,10 @@
-# Create a simple "Hello World" -bash script. This is to ensure that Bash is properly installed and working on your machine.
+# 2) Create a simple "Hello World" -bash script. This is to ensure that Bash is properly installed and working on your machine.
 
 #!/bin/bash
 echo hello world
 
 
-# Develop a bash script that takes an IP (IPv4) from a user. Based on this IP, the script should ping all (255) IPs within that range. For example, if the user enters the IP 192.168.1.50, the script should ping every IP from 192.168.1.1 to 192.168.1.255 *Hint:* use read to get input from the user.
+# 3) Develop a bash script that takes an IP (IPv4) from a user. Based on this IP, the script should ping all (255) IPs within that range. For example, if the user enters the IP 192.168.1.50, the script should ping every IP from 192.168.1.1 to 192.168.1.255 *Hint:* use read to get input from the user.
 #!/bin/bash
 read -p "Enter beginning IP-address: " host1
 read -p "Enter ending IP-address: " host2
@@ -15,7 +15,7 @@ for ((i=netId1; i<=netId2; i++)); do
 ping -c 1 ${SUBNET}.$i
 done
 
-# Update the script so that the script only prints the IPs of which there was a reply. *Hint:* It might be easier to temporarily store the results in a file
+# 4) Update the script so that the script only prints the IPs of which there was a reply. *Hint:* It might be easier to temporarily store the results in a file
 #!/bin/bash
 prefix=${1:-192.168.1}
 
@@ -24,3 +24,8 @@ do
   ( ping -c1 -w1 ${prefix}.${ip} > /dev/null 2>&1 && echo ${prefix}.${ip} ) &
 done
 wait
+
+# 5) Update the script so that the user can use command-line arguments to ping an IP
+#!/bin/bash
+read -p "Enter IP-adress: " host3
+ping -c 1 $host3
