0694af7e9f76624b6d05b654b95168d076e06f18
Yaguel <yaguev17@hotmail.com>
2019-08-23 10:45:41 +0200

    First commit
    Man assignment 1 scripting

---
+++ IP_bash_script.sh
@@ -0,0 +1,20 @@
+# Create a simple "Hello World" -bash script. This is to ensure that Bash is properly installed and working on your machine.
+
+#!/bin/bash
+echo hello world
+
+
+# Develop a bash script that takes an IP (IPv4) from a user. Based on this IP, the script should ping all (255) IPs within that range. For example, if the user enters the IP 192.168.1.50, the script should ping every IP from 192.168.1.1 to 192.168.1.255 *Hint:* use read to get input from the user.
+
+#!/bin/bash
+prefix=${1:-192.168.1}
+
+for ip in `seq 1 255`
+do
+  ( echo ping -c1 -w1 ${prefix}.${ip} > /dev/null 2>&1 && echo ${prefix}.${ip} ) &
+done
+wait
+
+# Update the script so that the script only prints the IPs of which there was a reply. *Hint:* It might be easier to temporarily store the results in a file
+
+#!/bin/bash
